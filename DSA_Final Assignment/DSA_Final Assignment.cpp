ff701aff48551e2106c4f431bd9d63cb8660346f
toyocrabs <mljkprvt@gmail.com>
2022-01-21 16:33:25 +0800

    Added datetimeapp and basic menu

--- DSA_Final Assignment/DSA_Final Assignment.cpp
+++ DSA_Final Assignment/DSA_Final Assignment.cpp
@@ -4,6 +4,49 @@ using namespace std;
 
 int main()
 {
-    cout << "Test first commit by hakimi\n";
-    cout << "Test by jun keat\n";
+	int option = 1;
+    while (option != 0)
+	{
+		displayMenu();
+		cin >> option;
+
+		if (option == 1) // display items
+		{
+			cout << "Guest Check In" << endl;
+		}
+		else
+		if (option == 2)	// search item
+		{
+			cout << "Add new booking" << endl; 
+		}
+		else
+		if (option == 3)	// add item
+		{
+			cout << "Display guests" << endl;
+		}
+		else
+		if (option == 4)	// delete item
+		{
+			cout << "Display occupied rooms" << endl;
+		}
+		if (option == 0)
+			cout << "Bye! \n";
+		else
+			cout << "Sorry. You have entered an invalid option. \n";
+	}
+
+}
+
+void displayMenu()
+{
+	cout << endl;
+	cout << "Hotel App \n";
+	cout << "--------------------------------\n";
+	cout << "1 Guest Check In\n";
+	cout << "2 Add New Booking            \n";
+	cout << "3 Display Guests               \n";
+	cout << "4 Display Occupied Rooms               \n";
+	cout << "0 Exit							 \n";
+	cout << "--------------------------------\n";
+	cout << "Enter option : ";
 }
---
+++ DSA_Final Assignment/DateTimeApp.cpp
@@ -0,0 +1,21 @@
+// DateTimeApp.cpp : Converts a string to a time structure (tm) using sscanf_s() function
+//
+
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{   // https://www.cplusplus.com/reference/ctime/tm/ for info about time structure (tm)
+    tm result;
+    char aString[] = "03/04/2020  06:09:02";
+    sscanf_s(aString, "%d/%d/%4d  %d:%d:%d",
+        &result.tm_mday, &result.tm_mon, &result.tm_year, &result.tm_hour, &result.tm_min, &result.tm_sec);
+    printf("tm_hour:  %d\n", result.tm_hour);
+    printf("tm_min:  %d\n", result.tm_min);
+    printf("tm_sec:  %d\n", result.tm_sec);
+    printf("tm_mday:  %d\n", result.tm_mday);
+    printf("tm_mon:  %d\n", result.tm_mon);
+    printf("tm_year:  %d\n", result.tm_year);
+}
